c8e2086abac2b35d074f9bd7dec8a17096f361f3
ricardohorie07 <ricardohorie07@gmail.com>
2026-04-30 13:38:54 -0300

    projeto inicial android

---
+++ app/src/main/java/com/ricardo/app/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.ricardo.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        setContent {
+            MaterialTheme {
+                TelaPDV()
+            }
+        }
+    }
+}
+
+@Composable
+fun TelaPDV() {
+    var quantidade by remember { mutableStateOf(0) }
+    val preco = 10.0
+    val total = quantidade * preco
+
+    Scaffold { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(text = "PDV - Caixa")
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(text = "Produto: Café")
+            Text(text = "Preço: R$ $preco")
+            Text(text = "Quantidade: $quantidade")
+            Text(text = "Total: R$ $total")
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(onClick = { quantidade++ }) {
+                Text("Adicionar Café")
+            }
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(onClick = { quantidade = 0 }) {
+                Text("Limpar venda")
+            }
+        }
+    }
+}
